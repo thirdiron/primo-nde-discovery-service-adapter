a4e3a990e2ea2c3e183e0f3ad19c00c3bb17c9a3
Danielle Books <danielle@thirdiron.com>
2025-12-12 16:32:29 -0700

    remove logging

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpResponse,
-} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { ApiResult, ArticleData, JournalData } from '../types/tiData.types';
 import { ConfigService } from './config.service';
@@ -20,37 +16,29 @@ export class HttpService {
   private apiUrl = this.configService.getApiUrl();
   private apiKey = this.configService.getApiKey();
 
-  constructor(private http: HttpClient, private configService: ConfigService) {
-    console.log('apiUrl', this.apiUrl);
-    console.log('apiKey', this.apiKey);
-  }
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService
+  ) {}
 
   getArticle(doi: string): Observable<any> {
     const endpoint = `${
       this.apiUrl
     }/articles/doi/${doi}?include=journal,library${this.appendAccessToken()}`;
 
-    return this.http
-      .get(endpoint, { observe: 'response' })
-      .pipe(catchError(this.handleError));
+    return this.http.get(endpoint, { observe: 'response' }).pipe(catchError(this.handleError));
   }
 
   getJournal(issn: string): Observable<any> {
-    const endpoint = `${
-      this.apiUrl
-    }/search?issns=${issn}${this.appendAccessToken()}`;
-    return this.http
-      .get(endpoint, { observe: 'response' })
-      .pipe(catchError(this.handleError));
+    const endpoint = `${this.apiUrl}/search?issns=${issn}${this.appendAccessToken()}`;
+    return this.http.get(endpoint, { observe: 'response' }).pipe(catchError(this.handleError));
   }
 
   getUnpaywall(doi: string): Observable<HttpResponse<Object>> {
     const email = this.configService.getEmailAddressKey();
 
     const endpoint = `https://api.unpaywall.org/v2/${doi}?email=${email}`;
-    return this.http
-      .get(endpoint, { observe: 'response' })
-      .pipe(catchError(this.handleError));
+    return this.http.get(endpoint, { observe: 'response' }).pipe(catchError(this.handleError));
   }
 
   getData(response: ApiResult): ArticleData | JournalData | {} {
@@ -97,10 +85,7 @@ export class HttpService {
 
   private handleError(error: HttpErrorResponse) {
     // Return an observable with a user-facing error message.
-    console.error(
-      `Backend returned code ${error.status}, body was: `,
-      error.error
-    );
+    console.error(`Backend returned code ${error.status}, body was: `, error.error);
 
     return throwError(
       () =>
